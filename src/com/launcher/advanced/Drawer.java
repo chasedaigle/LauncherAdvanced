0ac21b31a6f4c4d39c8f719d5278669c40ad1875
Esrom <esromgalaxy5@gmail.com>
2012-12-01 17:02:27 -0200

    source launcheradvanced by EsromG5

---
+++ src/com/launcher/advanced/Drawer.java
@@ -0,0 +1,29 @@
+package com.launcher.advanced;
+
+
+public interface Drawer {
+
+	public int getVisibility();
+
+	public boolean isOpaque();
+
+	public boolean hasFocus();
+
+	public boolean requestFocus();
+
+	public void setTextFilterEnabled(boolean textFilterEnabled);
+	public void clearTextFilter();
+
+
+	public void setDragger(DragController dragger);
+	public void setLauncher(Launcher launcher);
+	public void updateAppGrp();
+	public void setNumColumns(int numColumns);
+	public void setNumRows(int numRows);
+	public void setPageHorizontalMargin(int margin);
+	public void setAdapter(ApplicationsAdapter adapter);
+	public void setAnimationSpeed(int speed);
+	public void open(boolean animate);
+	public void close(boolean animate);
+	public void setPadding(int left, int top, int right, int bottom);
+}
